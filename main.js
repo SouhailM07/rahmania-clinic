01cc1b7789fa13fa582ad2fa55c3e4bbd975052d
souhail <lightshadow416@gmail.com>
2025-06-23 11:39:03 +0100

    updated things

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let menu_list = document.querySelector("#menu-list")
+menu_list.addEventListener("click", (e) => {
+    menu_list.classList.toggle("visually-hidden")
+})
+let menu_btn = document.querySelector("#menu-btn").addEventListener("click", () => {
+    menu_list.classList.toggle("visually-hidden")
+})
 window.addEventListener('scroll', () => {
     const navbar = document.querySelector('header');
     // console.log('Scroll event triggered', navbar);
@@ -9,3 +16,4 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('scrolled');
     }
 });
+
